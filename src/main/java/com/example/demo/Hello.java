ebe0078f481ba1a534a1f42f64c9612d3f22cab1
Luis Rojas <luisrojas@bcp.com.pe>
2022-09-27 18:10:16 -0500

    integrate with azure key vault and managed identity

--- src/main/java/com/example/demo/Hello.java
+++ src/main/java/com/example/demo/Hello.java
@@ -11,6 +11,16 @@ import org.springframework.web.client.RestTemplate;
 import reactor.core.publisher.Mono;
 
 import java.util.function.Function;
+import org.springframework.beans.factory.annotation.Value;
+
+//access azure key vault
+import com.azure.core.util.polling.SyncPoller;
+import com.azure.identity.DefaultAzureCredentialBuilder;
+
+import com.azure.security.keyvault.secrets.SecretClient;
+import com.azure.security.keyvault.secrets.SecretClientBuilder;
+import com.azure.security.keyvault.secrets.models.DeletedSecret;
+import com.azure.security.keyvault.secrets.models.KeyVaultSecret;
 
 @Component
 @Slf4j
@@ -19,6 +29,8 @@ public class Hello implements Function<Mono<User>, Mono<Greeting>> {
     @Autowired
     RestTemplate restTemplate;
 
+  /*   @Value("${secreto}")
+    private String secreto; */
 
     public Mono<Greeting> apply(Mono<User> mono) {
 
@@ -26,6 +38,17 @@ public class Hello implements Function<Mono<User>, Mono<Greeting>> {
 
         log.info(hello.toString());
 
-        return mono.map(user -> new Greeting("Hello, " + user.getName() + "!"));
+        String secreto="";
+       // String keyVaultName = System.getenv("KEY_VAULT_NAME");
+        String keyVaultUri = "https://akvlrojastest.vault.azure.net";
+
+        SecretClient secretClient = new SecretClientBuilder()
+            .vaultUrl(keyVaultUri)
+            .credential(new DefaultAzureCredentialBuilder().build())
+            .buildClient();
+
+        KeyVaultSecret retrievedSecret = secretClient.getSecret("secreto01");
+
+        return mono.map(user -> new Greeting("Hello, " + user.getName() + "! Secreto: " + retrievedSecret.getValue()));
     }
 }
